10c0af6ba8a61bfe54d449a862485eca693a25dd
kt <kt@kt.local>
2025-08-21 14:36:16 +0900

    Fix base path for GitHub Pages - Thu Aug 21 14:36:16 JST 2025

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2110e485ac19e1cb6a3216c2418c5717",
+const RESOURCES = {"flutter_bootstrap.js": "1a0653478f4c3d7dabd12cfaa0b86f30",
 "version.json": "5c0ae5c9eab5075da9eb4ceb29d31c30",
 "index.html": "570089d42552729195567064c9f50e07",
 "/": "570089d42552729195567064c9f50e07",
-"main.dart.js": "1d7e9f34d55f8b08888677eb9a9ed7f3",
+"main.dart.js": "cc3ca6f104ea9097abb26b5279c0b53c",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
